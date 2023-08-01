f70cc1f52eb5b996d0dba702881e8374c3312b3f
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-08-01 15:58:37 +0300

    add parentheses to return statements

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,7 +15,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	height_l = binary_tree_height(tree->left);
 	height_r = binary_tree_height(tree->right);
 	if (height_l > height_r)
-		return height_l + 1;
+		return (height_l + 1);
 	else
-		return height_r + 1;
+		return (height_r + 1);
 }
