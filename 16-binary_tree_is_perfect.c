0ed86f50b057aef2f580d3ea97264d16e8543b91
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-08-03 09:09:47 +0300

    add 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -59,8 +59,12 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	left = binary_tree_is_full(tree->left);
-	right = binary_tree_is_full(tree->right);
+	if (tree->left && tree->right)
+	{
+		left = binary_tree_is_full(tree->left);
+		right = binary_tree_is_full(tree->right);
+		return (left && right);
+	}
 
-	return ((left + right) % 2 == 0);
+	return (0);
 }
